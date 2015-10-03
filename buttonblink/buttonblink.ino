1882038fd4934a385c9e7903d42291532d2ca141
mesanu <esanu@ualberta.ca>
2015-10-03 11:03:13 -0600

    led blinker

--- buttonblink/buttonblink.ino
+++ buttonblink/buttonblink.ino
@@ -1,17 +1,32 @@
 void setup() {
-  pinMode(7,OUTPUT);
-  pinMode(2, INPUT);
+  pinMode(4,INPUT);
+  pinMode(2,INPUT);
+  pinMode(3,OUTPUT);
+  pinMode(5,OUTPUT);
+  pinMode(6,OUTPUT);
+  pinMode(9,OUTPUT);
+  
 
 }
 
 void loop() {
-  if (digitalRead(2)){
-    digitalWrite(7,1);
-  
-  }
-  else{
-    digitalWrite(7,0);
-  }
+  int debounceDelay=10;
+  int time1;
+  int latch=0;
+  int time2;
+  int latch2=0;
+  int player1=0;
+  int player2=0;
+   if (digitalRead(2) && latch1==0){
+     time1=millis();
+     latch=1;
+     player1++;
+   }
+   if (time1-millis()<latchDelay){
+     latch=0;
+     
+   }
+   analogWrite(3,player1);
   
 
 }
---
+++ tugofled/tugofled.ino
@@ -0,0 +1,66 @@
+int debounceDelay=100;
+   int time1;
+   int latch1=0;
+   int time2;
+   int latch2=0;
+   int pos;
+unsigned char ledControlCurve(int offset,int x);
+
+void setup() {
+  pinMode(4,INPUT);
+  pinMode(2,INPUT);
+  pinMode(3,OUTPUT);
+  pinMode(5,OUTPUT);
+  pinMode(6,OUTPUT);
+  pinMode(9,OUTPUT);
+  Serial.begin(9600);
+  
+
+}
+
+void loop() {
+   
+   if (digitalRead(2) && (latch1==0)){
+     time1=millis();
+     latch1=1;
+     pos+=20;
+   }
+   if (((time1-millis())>debounceDelay)&& !digitalRead(2)){
+     latch1=0;
+   }
+   if (digitalRead(4) && (latch2==0)){
+     time2=millis();
+     latch2=1;
+     pos-=20;
+   }
+   if (((time2-millis())>debounceDelay)&& !digitalRead(4)){
+     latch2=0;
+   }
+   Serial.println(ledControlCurve(0,pos));
+   analogWrite(3,ledControlCurve(-1020,pos));
+   analogWrite(5,ledControlCurve(-510,pos));
+   analogWrite(6,ledControlCurve(0,pos));
+   analogWrite(9,ledControlCurve(510,pos));
+   analogWrite(10,ledControlCurve(1020,pos));
+   Serial.println(pos);
+   
+}
+unsigned char ledControlCurve(int offset, int x){
+  if ((x+offset)>255){
+    return 0;
+  }
+  if (((x+offset)>=0)&&((x+offset)<=255)){
+    return -1*(x+offset)+255;
+    
+  }
+  if (((x+offset)<0)&&((x+offset)>=-255)){
+    return ((x+offset)+255);
+    
+  }
+  if ((x+offset)<255){
+    return 0;
+    
+  }
+}
+
+
