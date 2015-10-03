cf6d9b4c8699f0d29ff3eb430338f7dcd8783a4c
eyesniper2 <eyesniper2@rayfall.net>
2015-10-03 13:18:34 -0600

    Added fading in to the LEDFadeInandOut example

--- LEDFadeInandOut/FadeInandOut.ino
+++ LEDFadeInandOut/FadeInandOut.ino
@@ -7,12 +7,19 @@ void setup() {
 
 void loop() {
   for(int i = 0; i < 255; i++){
+    analogWrite(pin1, 0+i);
+    delay(5);
+  }
+  for(int i = 0; i < 255; i++){
     analogWrite(pin1, 255-i);
+    delay(5);
+  }
+  for(int i = 0; i < 255; i++){
+    analogWrite(pin2, 255+i);
     delay(5);
   }
   for(int i = 0; i < 255; i++){
     analogWrite(pin2, 255-i);
     delay(5);
   }
-
 }
