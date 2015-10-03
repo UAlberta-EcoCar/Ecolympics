d00559819843559ef43948f8ec1e8599359fae1b
eyesniper2 <eyesniper2@rayfall.net>
2015-10-03 12:28:11 -0600

    Adding LED fade in and out example

---
+++ LEDFadeInandOut/FadeInandOut.ino
@@ -0,0 +1,18 @@
+int pin1 = 9;
+int pin2 = 10;
+
+
+void setup() {
+}
+
+void loop() {
+  for(int i = 0; i < 255; i++){
+    analogWrite(pin1, 255-i);
+    delay(5);
+  }
+  for(int i = 0; i < 255; i++){
+    analogWrite(pin2, 255-i);
+    delay(5);
+  }
+
+}
